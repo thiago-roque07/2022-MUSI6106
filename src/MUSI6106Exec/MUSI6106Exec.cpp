d3d1330f4db23d93ab6de42d506982beb4996f02
alexanderlerch <alexander.lerch@gatech.edu>
2022-02-23 12:41:43 -0500

    project for the spectrogram exercise

--- src/MUSI6106Exec/MUSI6106Exec.cpp
+++ src/MUSI6106Exec/MUSI6106Exec.cpp
@@ -5,34 +5,122 @@
 #include "MUSI6106Config.h"
 
 #include "AudioFileIf.h"
-#include "CombFilterIf.h"
 
 using std::cout;
 using std::endl;
 
 // local function declarations
-void    showClInfo ();
+void    showClInfo();
 
 /////////////////////////////////////////////////////////////////////////////////
 // main function
 int main(int argc, char* argv[])
 {
-    std::string sInputFilePath,                 //!< file paths
-                sOutputFilePath;
+    std::string             sInputFilePath,                 //!< file paths
+        sOutputFilePath;
 
-    static const int kBlockSize = 1024;
+    static const int        kBlockSize = 1024;
 
-    clock_t time = 0;
+    clock_t                 time = 0;
 
-    float **ppfAudioData = 0;
+    float** ppfAudioData = 0;
 
-    CAudioFileIf *phAudioFile = 0;
-    std::fstream hOutputFile;
+    CAudioFileIf* phAudioFile = 0;
+    std::fstream            hOutputFile;
     CAudioFileIf::FileSpec_t stFileSpec;
 
-    CCombFilterIf   *pInstance = 0;
-    
     showClInfo();
+
+    //////////////////////////////////////////////////////////////////////////////
+    // parse command line arguments
+    if (argc < 2)
+    {
+        cout << "Missing audio input path!";
+        return -1;
+    }
+    else
+    {
+        sInputFilePath = argv[1];
+        sOutputFilePath = sInputFilePath + ".txt";
+    }
+
+    //////////////////////////////////////////////////////////////////////////////
+    // open the input wave file
+    CAudioFileIf::create(phAudioFile);
+    phAudioFile->openFile(sInputFilePath, CAudioFileIf::kFileRead);
+    if (!phAudioFile->isOpen())
+    {
+        cout << "Wave file open error!";
+        CAudioFileIf::destroy(phAudioFile);
+        return -1;
+    }
+    phAudioFile->getFileSpec(stFileSpec);
+
+    //////////////////////////////////////////////////////////////////////////////
+    // open the output text file
+    hOutputFile.open(sOutputFilePath.c_str(), std::ios::out);
+    if (!hOutputFile.is_open())
+    {
+        cout << "Text file open error!";
+        CAudioFileIf::destroy(phAudioFile);
+        return -1;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////
+    // allocate memory
+    ppfAudioData = new float* [stFileSpec.iNumChannels];
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
+        ppfAudioData[i] = new float[kBlockSize];
+
+    if (ppfAudioData == 0)
+    {
+        CAudioFileIf::destroy(phAudioFile);
+        hOutputFile.close();
+        return -1;
+    }
+    if (ppfAudioData[0] == 0)
+    {
+        CAudioFileIf::destroy(phAudioFile);
+        hOutputFile.close();
+        return -1;
+    }
+
+    time = clock();
+
+    //////////////////////////////////////////////////////////////////////////////
+    // get audio data and write it to the output text file (one column per channel)
+    while (!phAudioFile->isEof())
+    {
+        // set block length variable
+        long long iNumFrames = kBlockSize;
+
+        // read data (iNumOfFrames might be updated!)
+        phAudioFile->readData(ppfAudioData, iNumFrames);
+
+        cout << "\r" << "reading and writing";
+
+        // write
+        for (int i = 0; i < iNumFrames; i++)
+        {
+            for (int c = 0; c < stFileSpec.iNumChannels; c++)
+            {
+                hOutputFile << ppfAudioData[c][i] << "\t";
+            }
+            hOutputFile << endl;
+        }
+    }
+
+    cout << "\nreading/writing done in: \t" << (clock() - time) * 1.F / CLOCKS_PER_SEC << " seconds." << endl;
+
+    //////////////////////////////////////////////////////////////////////////////
+    // clean-up (close files and free memory)
+    CAudioFileIf::destroy(phAudioFile);
+    hOutputFile.close();
+
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
+        delete[] ppfAudioData[i];
+    delete[] ppfAudioData;
+    ppfAudioData = 0;
 
     // all done
     return 0;
@@ -44,7 +132,8 @@ void     showClInfo()
 {
     cout << "MUSI6106 Assignment Executable" << endl;
     cout << "(c) 2014-2022 by Alexander Lerch" << endl;
-    cout  << endl;
+    cout << endl;
 
     return;
 }
+
