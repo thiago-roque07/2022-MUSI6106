6dd44a82a719f12b7226d18da15aa9aea85a53d9
thiago.roque <37585254+thiago-roque07@users.noreply.github.com>
2022-04-18 11:40:25 -0400

    Began to implement tests

--- src/Tests/Tests/Test_FastConv.cpp
+++ src/Tests/Tests/Test_FastConv.cpp
@@ -23,16 +23,57 @@ namespace fastconv_test {
     protected:
         void SetUp() override
         {
+            m_IrLength = 51;
+            m_ImpulseLength = 10;
+            m_Ir = new float[m_IrLength];
+            m_Impulse = new float[m_ImpulseLength];
+
+            for (int i = 0; i < m_IrLength; i++)
+            {
+                if (i < m_ImpulseLength) { m_Impulse[i] = 0; };
+                m_Ir[i] = std::rand();
+            }
+            m_Impulse[3] = 1;
+
+            // CFastConv::init();
+            
         }
 
         virtual void TearDown()
         {
+            delete[] m_Ir;
+            delete[] m_Impulse;
         }
 
         CFastConv *m_pCFastConv = 0;
+        float *m_Ir = 0;
+        float *m_Impulse = 0;
+
+        int m_IrLength = 0;
+        int m_ImpulseLength = 0;
     };
 
-    TEST_F(FastConv, EmptyTest)
+    TEST_F(FastConv, Identity_TD)
+    {
+    }
+
+    TEST_F(FastConv, FlushBuffer_TD)
+    {
+    }
+
+    TEST_F(FastConv, Blocksize_TD)
+    {
+    }
+
+    TEST_F(FastConv, Identity_FD)
+    {
+    }
+
+    TEST_F(FastConv, FlushBuffer_FD)
+    {
+    }
+
+    TEST_F(FastConv, Blocksize_FD)
     {
     }
 }
